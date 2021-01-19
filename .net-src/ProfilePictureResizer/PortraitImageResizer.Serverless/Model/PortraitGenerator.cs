21c4a8a3afd3591b2f97cbaf0bc5c93a8f6e9bff
Maxime Rouiller <maxime.rouiller@microsoft.com>
2021-01-19 13:05:41 -0500

    Working version for the cloud

---
+++ .net-src/ProfilePictureResizer/PortraitImageResizer.Serverless/Model/PortraitGenerator.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Azure.WebJobs;
+using OpenCvSharp;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Processing;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace PortraitImageResizer.Serverless.Model
+{
+    public class PortraitGenerator
+    {
+        private ExecutionContext context;
+
+        public PortraitGenerator(ExecutionContext context)
+        {
+            this.context = context;
+        }
+
+        public Stream GeneratePortrait(Stream fileStream)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                fileStream.CopyTo(memoryStream);
+                byte[] fileContent = memoryStream.ToArray();
+
+                Rect[] faces = DetectFace(fileContent);
+                if (faces.Length == 0)
+                {
+                    Console.WriteLine($"No faces detected.");
+                    return null;
+                }
+                Rect face = faces.Where(face => face.Height == faces.Max(x => x.Height)).First();
+
+                using (Image image = Image.Load(fileContent))
+                {
+                    double zoomRatio = 2.5;
+                    Rectangle cropRectangle = CalculateCropRectangleForFace(face, zoomRatio, image.Width, image.Height);
+
+                    image.Mutate(x => x.Crop(cropRectangle));
+
+                    MemoryStream outputStream = new MemoryStream();                    
+                    var encoder = new JpegEncoder()
+                    {
+                        Quality = 75 
+                    };
+
+                    image.Save(outputStream, encoder);
+                    outputStream.Position = 0;
+                    return outputStream;
+                }
+            }
+        }
+
+        private Rectangle CalculateCropRectangleForFace(Rect face, double zoomRatio, int originalImageWidth, int originalImageHeight)
+        {
+            int x = face.X - (int)Math.Ceiling(((face.Width * zoomRatio) - face.Width) / 2);
+            int y = face.Y - (int)Math.Ceiling(((face.Height * zoomRatio) - face.Height) / 2);
+            x = x <= 0 ? 0 : x;
+            y = y <= 0 ? 0 : y;
+
+            int width = (int)Math.Ceiling(face.Width * zoomRatio);
+            int height = (int)Math.Ceiling(face.Height * zoomRatio);
+            width = (width + x) > originalImageWidth ? originalImageWidth - x : width;
+            height = (height + y) > originalImageHeight ? originalImageHeight - y : height;
+
+            // fixing non-square pictures
+            if (width != height)
+            {
+                // I do not understand how this works. It just does.
+                if (width > height)
+                {
+                    int offset = (width - height) / 2;
+                    x += offset;
+                    width = height;
+                }
+                else
+                {
+                    int offset = (height - width) / 2;
+                    y += offset;
+                    height = width;
+                }
+            }
+            return new Rectangle(x, y, width, height);
+        }
+
+        private Rect[] DetectFace(byte[] fileContent)
+        {
+            var cascade = new CascadeClassifier(Path.Combine(context.FunctionAppDirectory, "data/haarcascade_frontalface_default.xml"));
+            using (var src = Mat.FromImageData(fileContent, ImreadModes.Color))
+            using (var gray = new Mat())
+            {
+                Cv2.CvtColor(src, gray, ColorConversionCodes.BGR2GRAY);
+
+                // Detect faces
+                Rect[] faces = cascade.DetectMultiScale(gray, 1.08, 2, HaarDetectionType.ScaleImage, new OpenCvSharp.Size(30, 30));
+
+                return faces;
+            }
+        }
+    }
+}
--- .net-src/ProfilePictureResizer/PortraitImageResizer.Serverless/PortraitImageResizerFunctions.cs
+++ .net-src/ProfilePictureResizer/PortraitImageResizer.Serverless/PortraitImageResizerFunctions.cs
@@ -3,32 +3,55 @@
 using System;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.EventGrid;
 using Microsoft.Azure.EventGrid.Models;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.IO;
+using PortraitImageResizer.Serverless.Model;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage;
+using System.Threading.Tasks;
 
 namespace PortraitImageResizer.Serverless
 {
     public static class PortraitImageResizerFunctions
     {
         [FunctionName(nameof(HandleNewPictures))]
-        public static void HandleNewPictures([EventGridTrigger]EventGridEvent eventGridEvent, ILogger log)
+        public static async Task HandleNewPictures([EventGridTrigger] EventGridEvent receivedEvent,
+            [Blob("{data.Url}", FileAccess.Read, Connection = "PortraitStorageConnection")] Stream input,
+            ExecutionContext context,
+            ILogger log)
         {
+            // TODO: fix the docs that suggested this:
             // /blobServices/default/containers/portaits/raw-images/blobs/
+
+            // TODO: when it should have been this:
             // /blobServices/default/containers/portraits/blobs/raw-images
 
-
-            log.LogInformation(eventGridEvent.Subject);
-            log.LogInformation(eventGridEvent.Data.ToString());
-
-            // todo: read image raw data
+            StorageBlobCreatedEventData eventData = JsonConvert.DeserializeObject<StorageBlobCreatedEventData>(receivedEvent.Data.ToString());
+            log.LogInformation(JsonConvert.SerializeObject(eventData));
 
             // todo: run our code on the picture
+            PortraitGenerator generator = new PortraitGenerator(context);
+            Stream portrait = generator.GeneratePortrait(input);
 
             // todo: save the new picture to a different folder
+
+            CloudStorageAccount storageAccount;
+            if (CloudStorageAccount.TryParse(Environment.GetEnvironmentVariable("PortraitStorageConnection"), out storageAccount))
+            {
+                var blobClient = storageAccount.CreateCloudBlobClient();
+                var container = blobClient.GetContainerReference("portraits");
+
+
+                var blob = container.GetBlockBlobReference($"generated/{Path.GetFileName(eventData.Url)}");
+                await blob.UploadFromStreamAsync(portrait);
+            }
         }
     }
 
-    
+
 }
 
